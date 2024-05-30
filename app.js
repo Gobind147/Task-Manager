2d67d20e325955c1f7f69a7e7dbd42bbaece8632
Gobind147 <gobindkumar19095@gmail.com>
2024-05-30 21:55:28 +0530

    controllers -  routerss update

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
 
+
+//middleware
+
+app.use(express.json)
+
 // routes
 app.get('/hello', (req,res) => {
     res.send('task manager app')
 })
  
+app.use('/api/v1/tasks', tasks)
 
 
 //app.get('/api/v1/tasks')     - get all the tasks
