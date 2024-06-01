e2386943c0c3e9c55fbd2a4a3b6165e95c0d20e6
Gobind147 <gobindkumar19095@gmail.com>
2024-06-01 20:50:24 +0530

    update

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,14 @@
-const connectionString = 'mongodb+srv://gobind:<password>@task-manager.nfzafy6.mongodb.net/?retryWrites=true&w=majority&appName=task-manager'
+const mongoose = require('mongoose')
+
+const connectionString = 'mongodb+srv://gobind:4512@task-manager.nfzafy6.mongodb.net/?retryWrites=true&w=majority&appName=01-task-manager'
+
+
+mongoose
+    .connect(connectionString,{
+        useNewUrlParse: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(()=>console.log('connected to the db...'))
+    .catch((err)=>console.log(err))
